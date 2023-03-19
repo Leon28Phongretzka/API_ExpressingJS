70eda4b97e569583dba17e654d4806dfda7f6c48
Leon28Phongretzka <lamperougelelouchanm@gmail.com>
2023-03-19 10:39:39 +0700

    Delete method API

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -12,8 +12,8 @@ router.get('/', async (req, res) => {
 });
 
 // Getting One
-router.get('/:id', (req, res) => {
-
+router.get('/:id',getSubscriber, (req, res) => {
+    res.json(res.subscriber)
 });
 
 // Creating One
@@ -31,9 +31,54 @@ router.post('/', async (req, res) => {
     }
 });
 
-// Deleting One
-router.delete('/:id', (req, res) => {
+// Updating One
+router.patch('/:id',getSubscriber, async (req, res) => {
+    if(req.body.name != null)
+    {
+        res.subscriber.name = req.body.name;
+    }
+    if(req.body.subscriber != null)
+    {
+        res.subscriber.subscriberz = req.body.subscriber;
+    }
+    try{
+        const updatedSubscriber = await res.subscriber.save();
+        res.json(updatedSubscriber);
+    }
+    catch(err){
+        res.status(400).json({ message: err.message });
+    }
 
 });
+
+// Deleting One
+router.delete('/:id',getSubscriber, async (req, res) => {
+    try{
+        await res.subscriber.deleteOne();
+        res.json({ message: 'Deleted Subscriber' });
+    }
+    catch(err){
+        res.status(500).json({ message: err.message });
+    }
+});
+
+async function getSubscriber(req, res, next)
+{
+    let subscriber
+    try
+    {
+        subscriber = await Subscriber.findById(req.params.id);
+        if(subscriber == null)
+        {
+            return res.status(404).json({ message: 'Cannot find subscriber' });
+        }
+    }
+    catch(err)
+    {
+        return res.status(500).json({ message: err.message });
+    }
+    res.subscriber = subscriber
+    next()
+}
 
 module.exports = router
